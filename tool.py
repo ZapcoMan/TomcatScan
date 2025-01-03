b86e995523f1b60879d794ee9cd900269ece3eab
codervibe <3507557934@qq.com>
2025-01-03 14:56:53 +0800

    添加注释

--- tool.py
+++ tool.py
@@ -3,6 +3,10 @@
 # @Author  : codervibe
 # @File    : tool.py
 # @Project : TomcatWeakPasswordDetectionTool
+"""
+Tomcat弱密码检测工具的主要脚本。
+通过命令行参数接收输入，尝试检测Tomcat管理界面是否存在弱密码问题。
+"""
 import argparse
 import os
 
@@ -14,12 +18,14 @@ default_password_list = ["admin", "password", "123456"]
 
 # 创建ArgumentParser对象
 parser = argparse.ArgumentParser(description="Tomcat弱密码检测工具")
+# 添加命令行参数，包括URL、用户名字典文件和密码字典文件的路径
 parser.add_argument("-url", type=str, required=True, help="Tomcat管理界面的URL")
 parser.add_argument("-username_file", type=str, help="用户名字典文件的路径")
 parser.add_argument("-password_file", type=str, help="密码字典文件的路径")
 
 # 解析命令行参数
 args = parser.parse_args()
+# 获取命令行参数中的URL、用户名字典文件和密码字典文件的路径
 custom_url = args.url
 username_file = args.username_file
 password_file = args.password_file
@@ -38,13 +44,17 @@ if password_file and os.path.isfile(password_file):
 else:
     password_list = default_password_list
 
+# 实例化TomcatWeakPasswordDetector对象
 detector = TomcatWeakPasswordDetector(base_url=custom_url)
 
+# 遍历所有用户名和密码的组合，尝试登录Tomcat管理界面
 for username in username_list:
     for password in password_list:
         print(f"Trying username: {username}, password: {password}")
+        # 检测当前用户名和密码组合是否为弱密码
         is_weak = detector.check_weak_password(username, password)
         if is_weak:
+            # 如果检测到弱密码，输出相关信息
             print(f"\033[34m检测到弱密码。用户名: {username}, 密码: {password}\033[0m")
         # else:
         #     print(f"密码错误或认证失败。用户名: {username}, 密码: {password}")
