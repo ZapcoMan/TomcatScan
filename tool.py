c2c8074d85b9517eb2fb445fe9e97b4e1fe98d9e
codervibe <3507557934@qq.com>
2025-01-03 14:37:18 +0800

    添加 基本的提示

--- tool.py
+++ tool.py
@@ -3,9 +3,21 @@
 # @Author  : codervibe
 # @File    : tool.py
 # @Project : TomcatWeakPasswordDetectionTool
-from tomcatWeakPassword import tomcatWeakPassword
+from tomcatWeakPassword import TomcatWeakPasswordDetector
 
-username = []
-password = []
+# 用户名和密码列表
+username_list = ["admin", "user", "test"]
+password_list = ["admin", "password", "123456"]
 
-tomcatWeakPassword('admin','admin')
+
+
+# 指定URL
+custom_url = "http://localhost:8080/manager/html"
+detector = TomcatWeakPasswordDetector(base_url=custom_url)
+
+for username in username_list:
+    for password in password_list:
+        # print(f"Trying username: {username}, password: {password}")
+        is_weak = detector.check_weak_password(username, password)
+        if is_weak:
+            print(f"\033[34m检测到弱密码。用户名: {username}, 密码: {password}\033[34m")
