1d35be9e9cc6fda16b50f1d93e18680045e82cc2
codervibe <3507557934@qq.com>
2025-01-03 14:40:17 +0800

    修改代码 改为 参数输入 需要的 URL

--- tool.py
+++ tool.py
@@ -3,21 +3,27 @@
 # @Author  : codervibe
 # @File    : tool.py
 # @Project : TomcatWeakPasswordDetectionTool
+import argparse
+
 from tomcatWeakPassword import TomcatWeakPasswordDetector
 
 # 用户名和密码列表
 username_list = ["admin", "user", "test"]
 password_list = ["admin", "password", "123456"]
 
+# 创建ArgumentParser对象
+parser = argparse.ArgumentParser(description="Tomcat弱密码检测工具")
+parser.add_argument("-url", type=str, help="Tomcat管理界面的URL")
 
+# 解析命令行参数
+args = parser.parse_args()
+custom_url = args.url
 
-# 指定URL
-custom_url = "http://localhost:8080/manager/html"
 detector = TomcatWeakPasswordDetector(base_url=custom_url)
 
 for username in username_list:
     for password in password_list:
-        # print(f"Trying username: {username}, password: {password}")
+        print(f"Trying username: {username}, password: {password}")
         is_weak = detector.check_weak_password(username, password)
         if is_weak:
-            print(f"\033[34m检测到弱密码。用户名: {username}, 密码: {password}\033[34m")
+            print(f"\033[34m检测到弱密码。用户名: {username}, 密码: {password}\033[0m")
